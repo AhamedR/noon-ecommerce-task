553112c2ebbc3fd755f67202e72e0ea154206016
Ahamed Rasheed <ahamedrasheedhot@gmail.com>
2020-06-15 19:20:50 +0530

    Feat: Adding to favourites and removing from favourites feature implemented

--- src/components/Navigation/navigationBar.js
+++ src/components/Navigation/navigationBar.js
@@ -1,17 +1,21 @@
 ``
-export default function navigationBar({props}) {
+export default function navigationBar(props) {
   return (
     <nav className="nav">
         <div className="nav-menus">
-            <div>
-                <i class="fa fa-home" aria-hidden="true" i/>
+            <div
+                onClick={() => props.handleNavigation('HOME')}
+            >
+                <i className="fa fa-home" aria-hidden="true" ></i>
                 <span> Home </span>
             </div>
-            <div>
-                <i class="fa fa-heart" aria-hidden="true" i/>
+            <div
+                onClick={() => props.handleNavigation('FAV')}
+            >
+                <i className="fa fa-heart" aria-hidden="true" ></i>
                 <span> Favourites </span>
             </div>
         </div>
     </nav>
-  )
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,100 @@
+import React, { Component }  from 'react';
 import Layout from '../components/layout';
 import Product from '../components/product/product';
 import NavBar from '../components/Navigation/navigationBar';
 
-export default function Home({products}) {
-    return (
-        <Layout>
-            <NavBar/>
-            <div>
-                <Product
-                    allProducts={products}
+class Home extends Component {
+    state = {
+        isHomeView: true,
+        favourites: [],
+    }
+
+    /**
+     * Handle Favourites
+     * Add and remove favourites from state
+     * 
+     * @param {int} productId 
+     */
+    handleFavourite = (productId) => {
+        let {
+            favourites,
+        } = this.state;
+
+        if (favourites.includes(productId)) {
+            favourites = favourites.filter(function(favourite) {
+                return favourite !== productId
+            });
+        } else {
+            favourites.push(productId);
+        }
+
+        this.setState({
+            favourites,
+        });
+    }
+
+    /**
+     * Handle Navigation
+     * toggle trough Home and Favorites tab
+     * 
+     * @param {string} option 
+     */
+    handleNavigation = (option) => {
+        let {
+            isHomeView,
+        } = this.state;
+
+        switch (option) {
+            case 'HOME':
+                isHomeView = true;
+                break;
+
+            case 'FAV':
+                isHomeView = false;
+                break;
+
+            default:
+                break;
+        }
+
+        this.setState({
+            isHomeView,
+        });
+    }
+
+    render() {
+        const {
+            products,
+        } = this.props;
+
+        const {
+            favourites,
+            isHomeView,
+        } = this.state;
+
+        return (
+            <Layout>
+                <NavBar
+                    handleNavigation={this.handleNavigation}
                 />
-            </div>
-        </Layout>
-    );
+                <div>
+                    <Product
+                        isHomeView={isHomeView}
+                        favourites={favourites}
+                        handleFavourite={this.handleFavourite}
+                        allProducts={products}
+                    />
+                </div>
+            </Layout>
+        );
+    }
 }
 
 export async function getStaticProps(){
-    const res  = await fetch(`http://localhost:3000/api/products`);
-    const data = await res.json()
+    // Update the proxy with your host address
+    const proxy = 'http://localhost:3000';
+    const res   = await fetch(`${proxy}/api/products`);
+    const data  = await res.json()
 
     return {
         props: {
@@ -25,3 +102,5 @@ export async function getStaticProps(){
         }
     };
 }
+
+export default Home;
